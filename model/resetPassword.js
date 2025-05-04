75ecd972edc41628fbe994941c80395a724aac71
Naveen kumar <naveenkumarkalisetty20@gmail.com>
2025-05-04 19:33:02 +0530

    Delete model directory

--- model/resetPassword.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const resetPasswordToken = new mongoose.Schema({
-    email: {type: String, required: true},
-    token: {type: String, required: true}
-});
-
-
-module.exports =  mongoose.model('resetPasswordToken', resetPasswordToken) ;
